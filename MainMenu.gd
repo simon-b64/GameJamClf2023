0ad2bc13dd63c479995c8cba8cdd977b27475dd2
Moritz Patterer <moritz.patterer@cloudflight.io>
2023-09-22 21:50:36 +0200

    migrate to gdscript

---
+++ MainMenu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("BackgroundAnimation").play("default")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://GameScene.tscn")

4af354a2561fc491a7e09c5262137f721e465183
Moritz Patterer <moritz.patterer@cloudflight.io>
2023-09-22 23:17:18 +0200

    start with portal service

--- Door.gd
+++ Door.gd
@@ -1,6 +1,19 @@
 extends Area2D
 
+var player = null
 
-func _on_body_entered(body):
-	print("body entered")
+func _on_door_body_entered(body):
+	if body.get_name() != "Player":
+		return
+	player = body
+	
+func _on_door_body_exit(body):
+	if body.get_name() != "Player":
+		return
+	player = null
+
+func _process(delta):
+	if player == null:
+		return
+	
 	pass

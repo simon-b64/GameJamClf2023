1bf5f69cd55e3a60db73c8490cd06aa07ec0f015
Simon Lehner-Dittenberger <simon.lehner-dittenberger@cloudflight.io>
2023-09-23 21:01:51 +0200

    Make editor usable by hiding player fog

--- PlayerController.gd
+++ PlayerController.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	$Node2D.visible = true
 
 func _physics_process(delta):
 	# Add the gravity.

78b5f8e1477c2e5ed4b0e6c97e44e49753c4897a
Moritz Patterer <moritz.patterer@cloudflight.io>
2023-09-24 16:28:58 +0200

    camera

--- PlayerController.gd
+++ PlayerController.gd
@@ -2,11 +2,13 @@ extends CharacterBody2D
 
 @export var SPEED: int = 300.0
 @export var JUMP_VELOCITY: int = -900
+@export var TARGET_OFFSET: int = 100
 
 var MAX_Y_DOWN_VELOCITY = 1500
 var JUMP_BUCKET_CONST: float = 0.3
 var jump_bucket: int = JUMP_VELOCITY
 var delta_since_latst_vel_add: int = 0
+var last_direction: int
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -39,6 +41,7 @@ func _physics_process(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
+		last_direction = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
@@ -53,8 +56,23 @@ func _physics_process(delta):
 		else:
 			$PlayerAnimationTree.get("parameters/playback").travel("AirMovement")
 		
-
+	handle_camera(delta)
 	move_and_slide()
+
+func handle_camera(delta: float):
+	var screen_height = get_viewport_rect().size.y
+	var clamped_y_velocity = min(0.8, max(0.01, abs(velocity.y)/1500))
+	var target_position = Vector2(sign(last_direction) * TARGET_OFFSET, sign(velocity.y) * screen_height / 2 * clamped_y_velocity)
+	var camera_speed = Vector2(delta * (target_position.x - $Camera2D.offset.x) * 2, delta * (target_position.y - $Camera2D.offset.y) * 3)
+	if target_position.y > $Camera2D.offset.y:
+		$Camera2D.offset.y = min($Camera2D.offset.y + camera_speed.y, target_position.y)
+	else:
+		$Camera2D.offset.y = max($Camera2D.offset.y + camera_speed.y, target_position.y)
+	if target_position.x > $Camera2D.offset.x:
+		$Camera2D.offset.x = min($Camera2D.offset.x + camera_speed.x, target_position.x)
+	else:
+		$Camera2D.offset.x = max($Camera2D.offset.x + camera_speed.x, target_position.x)
+	print(target_position)
 
 func hide_thoughts():
 	$Book.visible = false
